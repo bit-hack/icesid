277ae5506f7b26c1a8f1012af71a23de0f9fbe15
Aidan <aidan.dodds@googlemail.com>
2021-08-30 23:32:25 +0100

    Rewrite the envelope

--- icesid/env.v
+++ icesid/env.v
@@ -1,15 +1,6 @@
 `default_nettype none
 `timescale 1us/1ns
 
-// theory of operation:
-// - ~1Mhz clocks LFSR15
-// - current rate setting (att/dec/rel) sets the compare value for LFSR15
-// - when next LFSR15 value has a compare match it resets
-// - when LFSR15 resets in ATTACK phase, the envelope increments
-// - when the envelope reaches 0xff it enters the DECAY phase
-// - if GATE goes low at any time it enters the RELEASE phase
-// - in the DEC/REL phase the env is clocked on LFSR5 reset
-// - when envelope hits a threshold, the LFSR5 reset compare value advances
 module sid_env_imp(
     input        CLK,
     input        CLKen,
@@ -21,188 +12,142 @@ module sid_env_imp(
     output [7:0] OUT
     );
 
-  reg [ 7:0] env;           // envelope output value
-  reg [14:0] lfsr15;        // 15 stage lfsr
-  reg [ 4:0] lfsr5;         // 5 stage lfsr
-  reg gate_lag;             // delayed gate signal
-  reg dir;                  // 0: down, 1: up
-
+  reg [7:0] env;    // envelope output value
   assign OUT = env;
 
+  localparam STAGE_ATT     = 1;
+  localparam STAGE_DEC_SUS = 2;
+  localparam STAGE_REL     = 4;
+  reg [2:0] stage;
+
   initial begin
-    env          = 0;
-    lfsr15       = 'h7fff;
-    lfsr5        = 'h1f;
-    gate_lag     = 0;
-    dir          = 0;
-    env_dec_cmp  = 0;
-    lfsr5_sel    = 1;
-    lfsr15_reset = 0;
-    lfsr5_reset  = 0;
-    lfsr15_cmp   = 0;
-    lfsr5_cmp    = 0;
-    stage_rate   = 0;
-    env_sus_cmp  = 0;
+    env     = 0;
+    stage   = STAGE_REL;
+    cnt     = 0;
+    cnt_rst = 0;
+    div     = 0;
+    div_max = 1;
+    div_rst = 0;
   end
 
   // envelope decay/release comparitor
-  reg env_dec_cmp;
+  reg [4:0] div_max;
   always @(posedge CLK) begin
-    env_dec_cmp <=
-//    env == 'hff |              //   1 (pseudo)
-      env == 'h5d |              //   2
-      env == 'h36 |              //   4
-      env == 'h1a |              //   8
-      env == 'h0e |              //  16
-      env == 'h06 |              //  30
-      env == 'h00;               // INF
+    case (env)
+    8'hff: div_max <=  1;
+    8'h5d: div_max <=  2;
+    8'h36: div_max <=  4;
+    8'h1a: div_max <=  8;
+    8'h0e: div_max <= 16;
+    8'h06: div_max <= 30;
+    8'h00: div_max <=  1;
+    default: ;
+    endcase
   end
 
-  reg [5:0] lfsr5_cmp;           // stores all the comparisons against lfsr5
-  reg [5:0] lfsr5_sel;           // select which bit of lfsr5_cmp to reset on
+  reg [3:0] rate;
   always @(*) begin
-    lfsr5_cmp = {
-      lfsr5_next == 'h0f,        //  30
-      lfsr5_next == 'h08,        //  16
-      lfsr5_next == 'h1b,        //   8
-      lfsr5_next == 'h11,        //   4
-      lfsr5_next == 'h1c,        //   2
-      lfsr5_next == 'h1e         //   1 (added)
-    };
+    case (stage)
+    STAGE_ATT:     rate = ATT;
+    STAGE_DEC_SUS: rate = DEC;
+    default:       rate = REL;
+    endcase
   end
 
-  reg [15:0] lfsr15_cmp;         // stores all the comparisons against lfsr15
+  reg [14:0] cnt_max;
   always @(*) begin
-    lfsr15_cmp = {
-                                 // count  attack  decay (x3)
-      lfsr15_next == 'h0a93,     // 31250    8s       24s
-      lfsr15_next == 'h7625,     // 19531    5s       15s
-      lfsr15_next == 'h77e2,     // 11719    3s        9s
-      lfsr15_next == 'h3840,     //  3906    1s        3s
-      lfsr15_next == 'h59b8,     //  3125  800ms       2.4s
-      lfsr15_next == 'h1848,     //  1953  500ms       1.5s
-      lfsr15_next == 'h0222,     //   976  250ms     750ms
-      lfsr15_next == 'h1212,     //   391  100ms     300ms
-      lfsr15_next == 'h500e,     //   312   80ms     240ms
-      lfsr15_next == 'h3800,     //   266   68ms     204ms
-      lfsr15_next == 'h6755,     //   219   56ms     168ms
-      lfsr15_next == 'h20c0,     //   148   38ms     114ms
-      lfsr15_next == 'h0330,     //    94   24ms      72ms
-      lfsr15_next == 'h003c,     //    62   16ms      48ms
-      lfsr15_next == 'h0006,     //    31    8ms      24ms
-      lfsr15_next == 'h7f00      //     8    2ms       6ms
-    };
+    case (rate)             // ATT      DEC/REL
+    4'hf: cnt_max = 31250;  //   8s       24s
+    4'he: cnt_max = 19531;  //   5s       15s
+    4'hd: cnt_max = 11719;  //   3s        9s
+    4'hc: cnt_max =  3906;  //   1s        3s
+    4'hb: cnt_max =  3125;  // 800ms       2.4s
+    4'ha: cnt_max =  1953;  // 500ms       1.5s
+    4'h9: cnt_max =   976;  // 250ms     750ms
+    4'h8: cnt_max =   391;  // 100ms     300ms
+    4'h7: cnt_max =   312;  //  80ms     240ms
+    4'h6: cnt_max =   266;  //  68ms     204ms
+    4'h5: cnt_max =   219;  //  56ms     168ms
+    4'h4: cnt_max =   148;  //  38ms     114ms
+    4'h3: cnt_max =    94;  //  24ms      72ms
+    4'h2: cnt_max =    62;  //  16ms      48ms
+    4'h1: cnt_max =    31;  //   8ms      24ms
+    4'h0: cnt_max =     8;  //   2ms       6ms
+    endcase
   end
 
-  // envelope sustain comparitor
-  reg env_sus_cmp;
+  reg [14:0] cnt;
+  reg cnt_rst;
   always @(posedge CLK) begin
-    env_sus_cmp <= (env == { SUS, SUS });
-  end
-
-  // update lfsr15 register
-  wire [14:0] lfsr15_next = { lfsr15[13:0], lfsr15[13] ^ lfsr15[14] };
-  reg lfsr15_reset;
-  always @(posedge CLK) begin
-    lfsr15_reset <= 0;
+    cnt_rst <= 0;
     if (CLKen) begin
-      if (lfsr15_cmp[stage_rate]) begin
-        // reset the lfsr
-        lfsr15_reset <= 1;
-        lfsr15 <= 'h7fff;
+      if (cnt == 0) begin
+        cnt_rst <= 1;
+        cnt <= cnt_max;
       end else begin
-        lfsr15 <= lfsr15_next;
+        cnt <= cnt - 1;
       end
     end
   end
 
-  // update lfsr5 register
-  wire [4:0] lfsr5_next = { lfsr5[3:0], lfsr5[2] ^ lfsr5[4] };
-  reg lfsr5_reset;
+  reg [4:0] div;
+  reg div_rst;
   always @(posedge CLK) begin
-    lfsr5_reset <= 0;
-    if (lfsr15_reset) begin
-      if (lfsr5_cmp & lfsr5_sel) begin
-        lfsr5_reset <= 1;
-        lfsr5 <= 'h1f;
+    div_rst <= 0;
+    if (cnt_rst) begin
+      if (div == 0) begin
+        div_rst <= 1;
+        div <= div_max;
       end else begin
-        lfsr5 <= lfsr5_next;
+        div <= div - 1;
       end
     end
   end
 
-  // update envelope direction
+  wire [7:0] sustain_val = { SUS, SUS };
+
   always @(posedge CLK) begin
-    gate_lag <= GATE;
-    if (GATE) begin
-      if (!gate_lag) begin
-        dir <= 1;               // up for attack
+    case (stage)
+    STAGE_ATT: begin
+      if (GATE) begin
+        if (env == 8'hff) begin
+          stage <= STAGE_DEC_SUS;
+        end else begin
+          if (cnt_rst) begin
+            env <= env + 1;
+          end
+        end
       end else begin
-        if (env == 'hff) begin
-          dir <= 0;             // down for decay phase
+        stage <= STAGE_REL;
+      end
+    end  // STAGE_ATT
+    STAGE_DEC_SUS: begin
+      if (GATE) begin
+        if (div_rst) begin
+          if (env == sustain_val) begin
+            // sustain so do nothing
+          end else begin
+            env <= (env == 0) ? 0 : (env - 1);
+          end
+        end
+      end else begin
+        stage <= STAGE_REL;
+      end
+    end  // STAGE_DEC_SUS
+    STAGE_REL: begin
+      if (GATE) begin
+        stage <= STAGE_ATT;
+      end else begin
+        if (div_rst) begin
+          env <= (env == 0) ? 0 : (env - 1);
         end
       end
-    end else begin
-      dir <= 0;                 // down for the release phase
-    end
-  end
-
-  // update the current stages rate selection
-  reg [3:0] stage_rate;
-  always @(posedge CLK) begin
-    case ({GATE, dir})
-    2'b00: stage_rate <= REL;
-    2'b01: stage_rate <= ATT;   // unused
-    2'b10: stage_rate <= DEC;
-    2'b11: stage_rate <= ATT;
+    end  // STAGE_REL
+    default:
+      stage <= STAGE_REL;
     endcase
   end
 
-  // update lfsr_sel to model the exponential decay
-  // when we get a env_dec_cmp we increase the period for the lfsr5 counter
-  always @(posedge CLK) begin
-    if (dir == 0) begin
-      if (lfsr5_reset) begin
-        if (env_dec_cmp) begin  // on compare match for env value
-          // shift left to
-          lfsr5_sel <= { lfsr5_sel[4:0], 1'b0 };
-        end
-      end
-    end else begin
-      lfsr5_sel <= 'b1;         // set to 1 which will reset lfsr5 to div/1
-    end
-  end
-
-  // update envelope
-  always @(posedge CLK) begin
-    case ({GATE, dir})
-    2'b00: begin                // release
-      if (lfsr5_reset) begin
-        env <= (env == 0) ? 0 : env - 'd1;
-      end
-    end
-    2'b10: begin                // decay
-      if (env_sus_cmp) begin
-        // sustaining, do nothing
-      end else begin
-        if (lfsr5_reset) begin
-
-          // note: we used to wrap around here if sustain was increased while we
-          //       were in the sustain phase. Once it wraped around it would
-          //       lock up as env_dec_cmp will go to infinite!  the real sid holds
-          //       at the zero value in this case, so we do the same.
-
-          env <= (env == 0) ? 0 : env - 'd1;
-        end
-      end
-    end
-    2'b11: begin                // attack
-      if (lfsr15_reset) begin
-        env <= env + 'd1;
-      end
-    end
-    endcase
-  end
 endmodule
 
 module sid_env(
@@ -237,7 +182,7 @@ module sid_env(
     reg_gate = 0;
     reg_att  = 0;
     reg_dec  = 0;
-    reg_sus  = 'hf;      // 100% sustain
+    reg_sus  = 0;
     reg_rel  = 0;
   end
 
