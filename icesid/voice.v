86b78a7c5d293770171ace8de38b72eeb743bfb4
Aidan <aidan.dodds@googlemail.com>
2022-01-18 21:29:03 +0000

    Dont invert voice waveform

--- icesid/voice.v
+++ icesid/voice.v
@@ -24,15 +24,15 @@ module sid_voice (
   parameter BASE_ADDR = 0;
 
   // voice related internal registers
-  reg [15:0] regFreq;  // frequency
-  reg [11:0] regPW;  // pulse width
-  reg        regNoise;  // wave-select noise enable
-  reg        regPulse;  // wave-select pulse enable
-  reg        regSaw;  // wave-select saw enable
-  reg        regTri;  // wave-select triangle enable
-  reg        regTest;  // test register
-  reg        regRingMod;  // ring modulate
-  reg        regSync;  // oscillator sync
+  reg [15:0] regFreq;    // frequency
+  reg [11:0] regPW;      // pulse width
+  reg        regNoise;   // wave-select noise enable
+  reg        regPulse;   // wave-select pulse enable
+  reg        regSaw;     // wave-select saw enable
+  reg        regTri;     // wave-select triangle enable
+  reg        regTest;    // test register
+  reg        regRingMod; // ring modulate
+  reg        regSync;    // oscillator sync
 
   // initial conditions
   initial begin
@@ -105,14 +105,13 @@ module sid_voice (
   // todo: the data sheet says the waveforms are "ANDed" together but that is
   //       not what happens. its much more complex than that, but for now lets
   //       do this and revise it later.
-  // note: we invert here so that when all channels are off a zero is produced.
   reg [11:0] wavMix;
-  assign oOut = ~wavMix;
+  assign oOut = wavMix;
   always @(posedge clk) begin
-    wavMix <= (regSaw   ? wavSaw   : 12'hfff) &
-              (regPulse ? wavPulse : 12'hfff) &
-              (regTri   ? wavTri   : 12'hfff) &
-              (regNoise ? wavNoise : 12'hfff);
+    wavMix <= (regSaw   ? wavSaw   : 12'h000) |
+              (regPulse ? wavPulse : 12'h000) |
+              (regTri   ? wavTri   : 12'h000) |
+              (regNoise ? wavNoise : 12'h000);
   end
 
   // address/data decoder
